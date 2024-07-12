23a2e6b8bfd8253a31d07211dc8ba19244a710b8
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-12 20:00:57 +0530

    Fibonacci series

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+
+            Console.Write(a + " " + b + " ");
+
+            for(int i=2; i<=n; i++)
+            {
+                int c = a + b;
+                Console.Write(c + " ");
+
+                a = b; b = c;
+            }
+            Console.ReadLine();
+        }
+    }
+}
